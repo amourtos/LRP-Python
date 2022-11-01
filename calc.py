56246353e2ebf856a3fa05b02273a32603c0ada5
Alex <amourtos@gmail.com>
2022-11-01 15:45:02 -0400

    added some comments

--- calc.py
+++ calc.py
@@ -2,14 +2,36 @@
 
 
 def addition(x , y):
+    # your code here
+
     return
 
 def subtraction(x , y):
+    # your code here
+
     return
 
+"""
+Do not use mathematical operators for these functions.
+The goal will be utilizing the first two functions to complete the operations.
+Think arithmetic: multiplication is addition multiple times.
+Resources for research will be provided.
+"""
 def multiply(x , y):
+    # HINT: Research how to write a for loop for this function.
+    # your code here
+
     return
 
 def divide(x , y):
+    # your code here
+
     return
     
+
+    """
+    Resources:
+    https://www.w3schools.com/python/python_for_loops.asp
+     - tip: refer at the for x in range() series of loops for this exercise
+
+    """
